b746f8e8e72e87ff52170849b00f7a884ad13bf8
MANOJ CHOUDHARY V <manojsasi20032005@gmail.com>
2024-07-13 09:25:48 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,77 @@
+const logotext = "MANOJ CHOUDHARY V";
+const meta = {
+    title: "Manoj Choudhary V",
+    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Manoj Choudhary V",
+    animated: {
+        first: "I love coding",
+        second: "",
+        third: "I develop mobile apps",
+    },
+    description: "I am a backend developer specializing in building scalable and efficient systems. My expertise includes database management, API integration, and optimizing performance for seamless user experiences",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "About My Self",
+    aboutme: "I am a backend developer specializing in building scalable and efficient systems. My expertise includes database management, API integration, and optimizing performance for seamless user experiences",
+};
+const worktimeline = [
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Java",
+        value: 85,
+    },
+    {
+        name: "SQL",
+        value: 85,
+    },
+];
+
+const services = [{}
+];
+
+const dataportfolio = [{
+        img: "https://www.istockphoto.com/vector/intelligent-technology-hud-vector-interface-network-management-data-screen-with-gm909118634-250410809",
+        description: "Analyzing weather data: charts and graphs display temperature trends, precipitation amounts, wind speeds, and atmospheric conditions for informed decisions.",
+        link: "https://github.com/manojvenaram/TIMESERIESPROJECT",
+    },
+    {
+        img: "https://www.google.com/imgres?q=Age%20and%20Gender%20Prediction%20images&imgurl=https%3A%2F%2Fmiro.medium.com%2Fv2%2Fresize%3Afit%3A651%2F1*0oucu2Lto_FxhNDHAomKfg.png&imgrefurl=https%3A%2F%2Fmedium.datadriveninvestor.com%2Fdata-science-gender-and-age-prediction-project-8ebc65ee3109&docid=DmP2j1SedCjZWM&tbnid=YFFkEN1WBuXe8M&vet=12ahUKEwiMhd2biaOHAxXVT2cHHY7NCzEQM3oECDQQAA..i&w=651&h=543&hcb=2&ved=2ahUKEwiMhd2biaOHAxXVT2cHHY7NCzEQM3oECDQQAA",
+        description: "Age and Gender Prediction",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "manojasasi20032005@gmail.com",
+    YOUR_FONE: "9360247533",
+    description: "I am a hard-working and driven individual who isn't afraid to face a challenge. I'm passionate about my work and I know how to get the job done. I would describe myself as an open and honest person who doesn't believe in misleading other people and tries to be fair in everything I do.",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
